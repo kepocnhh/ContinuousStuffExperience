20ab9565b518d84993ff383a5a563b8c2d1c22b1
Stanley Wintergreen <kepocnhh@gmail.com>
2019-09-18 15:34:06 +0300

    fix sh 004

--- gh-pages-report.sh
+++ gh-pages-report.sh
@@ -1,10 +1,16 @@
 git clone --depth=1 --branch=gh-pages https://github.com/${TRAVIS_REPO_SLUG}.git ~/gh-pages
-echo "clone: ${TRAVIS_REPO_SLUG}"
+
+git -C ~/gh-pages config user.name "${USER}"
+git -C ~/gh-pages config user.email "${USER}"
+
 ls -a ~/gh-pages
+
 sha512=$(sha512sum "${TRAVIS_BUILD_DIR}/build/reports/coverage/testCoverageReport.xml" | cut -d ' ' -f 1)
-echo "sha512 = ${sha512}"
+
 mkdir -p ~/gh-pages/reports/${sha512}/coverage
-echo "mkdir: ~/gh-pages/reports/${sha512}/coverage"
 mv ${TRAVIS_BUILD_DIR}/build/reports/coverage/* ~/gh-pages/reports/${sha512}/coverage
-echo "move ->"
-ls -a ~/gh-pages/reports/${sha512}/coverage
+
+git -C ~/gh-pages status
+git -C ~/gh-pages add --all .
+git -C ~/gh-pages commit -m "add ${sha512} test coverage report"
+git -C ~/gh-pages push -f -u "https://${git_hub_personal_access_token}@github.com/${TRAVIS_REPO_SLUG}" gh-pages
