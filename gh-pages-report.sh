adf6ece1b60b0dcb21a11824c7e92da00466469e
Stanley Wintergreen <kepocnhh@gmail.com>
2019-09-18 18:34:47 +0300

    fix sh 006

--- gh-pages-report.sh
+++ gh-pages-report.sh
@@ -1,3 +1,4 @@
+echo "clonong ${TRAVIS_REPO_SLUG} to ~/gh-pages..."
 git clone --depth=1 --branch=gh-pages https://github.com/${TRAVIS_REPO_SLUG}.git ~/gh-pages
 
 git -C ~/gh-pages config user.name "${USER}"
@@ -5,10 +6,17 @@ git -C ~/gh-pages config user.email "${USER}"
 
 signature=$(<"${TRAVIS_BUILD_DIR}/build/reports/coverage/signature")
 
+if [ -d "~/gh-pages/reports/${signature}/coverage" ]
+then
+    echo "test coverage report by ${signature} already in gh-pages"
+    exit
+fi
+
+echo "make dir ~/gh-pages/reports/${signature}/coverage..."
 mkdir -p ~/gh-pages/reports/${signature}/coverage
+echo "move test coverage report"
 mv ${TRAVIS_BUILD_DIR}/build/reports/coverage/* ~/gh-pages/reports/${signature}/coverage
 
-git -C ~/gh-pages status
 git -C ~/gh-pages add --all .
 git -C ~/gh-pages commit -m "add ${signature} test coverage report"
 git -C ~/gh-pages push -f -q -u "https://${git_hub_personal_access_token}@github.com/${TRAVIS_REPO_SLUG}" gh-pages
