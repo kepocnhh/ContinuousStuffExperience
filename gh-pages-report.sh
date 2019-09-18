b86317c699fd9fc22575c8eb38bd7a33b3fd560a
Stanley Wintergreen <kepocnhh@gmail.com>
2019-09-18 14:55:08 +0300

    fx sh 001

--- gh-pages-report.sh
+++ gh-pages-report.sh
@@ -1,20 +1,18 @@
-
-git clone --depth=1 --branch=gh-pages https://github.com/${TRAVIS_REPO_SLUG}.git gh-pages/${TRAVIS_REPO_SLUG}
-
+echo "now:"
+ls -a
+echo "root:"
+ls -a /
+echo "user:"
+ls -a ~
+echo "TRAVIS_BUILD_DIR(${TRAVIS_BUILD_DIR}):"
+ls -a ~
+git clone --depth=1 --branch=gh-pages https://github.com/${TRAVIS_REPO_SLUG}.git ${TRAVIS_BUILD_DIR}/gh-pages
 echo "clone: ${TRAVIS_REPO_SLUG}"
-
-ls -a /gh-pages/${TRAVIS_REPO_SLUG}/
-
-sha512=$(sha512sum "/${TRAVIS_REPO_SLUG}/build/reports/coverage/testCoverageReport.xml" | cut -d ' ' -f 1)
-
+ls -a ${TRAVIS_BUILD_DIR}/gh-pages
+sha512=$(sha512sum "${TRAVIS_BUILD_DIR}/${TRAVIS_REPO_SLUG}/build/reports/coverage/testCoverageReport.xml" | cut -d ' ' -f 1)
 echo "sha512 = ${sha512}"
-
-mkdir -p /gh-pages/${TRAVIS_REPO_SLUG}/reports/${sha512}/coverage
-
-echo "mkdir: /gh-pages/${TRAVIS_REPO_SLUG}/reports/${sha512}/coverage"
-
-mv /${TRAVIS_REPO_SLUG}/build/reports/coverage/* /gh-pages/${TRAVIS_REPO_SLUG}/reports/${sha512}/coverage
-
+mkdir -p ${TRAVIS_BUILD_DIR}/gh-pages/reports/${sha512}/coverage
+echo "mkdir: ${TRAVIS_BUILD_DIR}/gh-pages/reports/${sha512}/coverage"
+mv ${TRAVIS_BUILD_DIR}/${TRAVIS_REPO_SLUG}/build/reports/coverage/* ${TRAVIS_BUILD_DIR}/gh-pages/reports/${sha512}/coverage
 echo "move ->"
-
-ls -a /gh-pages/${TRAVIS_REPO_SLUG}/reports/${sha512}/coverage
+ls -a ${TRAVIS_BUILD_DIR}/gh-pages/reports/${sha512}/coverage
