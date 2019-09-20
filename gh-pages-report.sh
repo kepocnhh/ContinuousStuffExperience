c75a2844c48034711bd1de624937cd52bf7e5563
Stanley Wintergreen <kepocnhh@gmail.com>
2019-09-20 21:20:25 +0300

    automatic publish test report to gh-pages...

--- gh-pages-report.sh
+++ gh-pages-report.sh
@@ -4,20 +4,36 @@ git clone --depth=1 --branch=gh-pages https://github.com/${TRAVIS_REPO_SLUG}.git
 git -C ~/gh-pages config user.name "${USER}"
 git -C ~/gh-pages config user.email "${USER}"
 
-signature=$(<"${TRAVIS_BUILD_DIR}/build/reports/coverage/signature")
+testCoverageSignature=$(<"${TRAVIS_BUILD_DIR}/build/reports/coverage/signature")
+testCoveragePath="$HOME/gh-pages/reports/coverage/${testCoverageSignature}"
 
-path="$HOME/gh-pages/reports/coverage/${signature}"
-if [ -d $path ]
+if [ -d $testCoveragePath ]
 then
-    echo "test coverage report by ${signature} already in gh-pages"
-    exit
+    echo "test coverage report by ${testCoverageSignature} already in gh-pages"
+else
+	echo "make dir $testCoveragePath..."
+	mkdir -p $testCoveragePath
+	echo "move test coverage report"
+	mv ${TRAVIS_BUILD_DIR}/build/reports/coverage/html/* $testCoveragePath
+
+	git -C ~/gh-pages add --all .
+	git -C ~/gh-pages commit -m "add ${testCoverageSignature} test coverage report"
+	git -C ~/gh-pages push -f -q -u "https://${git_hub_personal_access_token}@github.com/${TRAVIS_REPO_SLUG}" gh-pages
 fi
 
-echo "make dir $path..."
-mkdir -p $path
-echo "move test coverage report"
-mv ${TRAVIS_BUILD_DIR}/build/reports/coverage/html/* $path
+testSignature=$(<"${TRAVIS_BUILD_DIR}/build/reports/tests/signature")
+testPath="$HOME/gh-pages/reports/tests/${testSignature}"
 
-git -C ~/gh-pages add --all .
-git -C ~/gh-pages commit -m "add ${signature} test coverage report"
-git -C ~/gh-pages push -f -q -u "https://${git_hub_personal_access_token}@github.com/${TRAVIS_REPO_SLUG}" gh-pages
+if [ -d $testCoveragePath ]
+then
+    echo "test report by ${testSignature} already in gh-pages"
+else
+	echo "make dir $testPath..."
+	mkdir -p $testPath
+	echo "move test report"
+	mv ${TRAVIS_BUILD_DIR}/build/reports/tests/html/* $testPath
+
+	git -C ~/gh-pages add --all .
+	git -C ~/gh-pages commit -m "add ${testSignature} test report"
+	git -C ~/gh-pages push -f -q -u "https://${git_hub_personal_access_token}@github.com/${TRAVIS_REPO_SLUG}" gh-pages
+fi
