fbd11f54a4118c5149d55d79b3395cbcc3edcb45
Stanley Wintergreen <kepocnhh@gmail.com>
2019-09-23 20:59:02 +0300

    fix telegram script

--- telegram-send-message.sh
+++ telegram-send-message.sh
@@ -1,14 +1,20 @@
 echo "telegram send message..."
 
+echo "1: ${#1}"
+echo "2: ${#2}"
+echo "3: ${#3}"
+echo "4: ${#4}"
+echo "5: ${#5}"
+
 if [ $# -ne 4 ]; then
-    echo "Script needs for 4 arguments"
+    echo "Script needs for 4 arguments but actual $#"
     exit 1
 fi
 
 TELEGRAM_BOT_ID=$1
 TELEGRAM_BOT_TOKEN=$2
 TELEGRAM_CHAT_ID=$3
-#MESSAGE=$4
+MESSAGE=$4
 
 url="https://api.telegram.org/bot$TELEGRAM_BOT_ID:$TELEGRAM_BOT_TOKEN/sendMessage"
 
@@ -18,10 +24,11 @@ RESPONSE_CODE=$(
 		$url \
 		-d chat_id=$TELEGRAM_CHAT_ID \
 		-d text="$MESSAGE" \
-		-d parse_mode=markdown || exit 2
+		-d parse_mode=markdown
 )
 
-if [ $RESPONSE_CODE -ne 200 ]
+if [ "$RESPONSE_CODE" != "200" ]
 then
+	echo "response code $RESPONSE_CODE but expected 200"
 	exit 3
 fi
