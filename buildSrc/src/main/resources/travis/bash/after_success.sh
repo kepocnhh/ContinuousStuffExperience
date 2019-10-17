248e47217ca3a6927648f36680ce55f45e97bb7d
Stanley Wintergreen <kepocnhh@gmail.com>
2019-10-17 18:55:27 +0300

    fix after success script

--- buildSrc/src/main/resources/travis/bash/after_success.sh
+++ buildSrc/src/main/resources/travis/bash/after_success.sh
@@ -2,9 +2,14 @@ echo "after success..."
 
 if [[ $TRAVIS_PULL_REQUEST =~ ^[0-9]+$ ]]
 then
-  if test "$TRAVIS_PULL_REQUEST_BRANCH" != "$DEVELOP_BRANCH_NAME"
+  if test "$TRAVIS_BRANCH" != "$DEVELOP_BRANCH_NAME"
   then
-    echo "it is not pull request to branch \"$DEVELOP_BRANCH_NAME\" but to branch \"$TRAVIS_PULL_REQUEST_BRANCH\""
+    echo "it is not pull request to branch \"$DEVELOP_BRANCH_NAME\" but to branch \"$TRAVIS_BRANCH\""
+    exit 0
+  fi
+  if test "$TRAVIS_PULL_REQUEST_BRANCH" == ""
+  then
+    echo "source branch of pull request #$TRAVIS_PULL_REQUEST undefined"
     exit 0
   fi
 else
@@ -12,14 +17,21 @@ else
   exit 0
 fi
 
-echo "it is pull request #$TRAVIS_PULL_REQUEST to branch \"$TRAVIS_PULL_REQUEST_BRANCH\""
+echo "it is pull request #$TRAVIS_PULL_REQUEST \"$TRAVIS_PULL_REQUEST_BRANCH\" -> \"$TRAVIS_BRANCH\""
 
 LOCAL_PATH="~/automerge"
 
 echo "cloning $TRAVIS_REPO_SLUG to $LOCAL_PATH..."
-git clone --depth=1 --branch="$DEVELOP_BRANCH_NAME" $REPO_URL.git $LOCAL_PATH || exit 1
+git clone --depth=1 --branch="$TRAVIS_BRANCH" $REPO_URL.git $LOCAL_PATH || exit 1
 
-git -C ~/gh-pages config user.name "$USER"
-git -C ~/gh-pages config user.email "$USER"
+git -C $LOCAL_PATH config user.name "$USER"
+git -C $LOCAL_PATH config user.email "$USER"
 
-echo "after success WIP"
+PULL_REQUEST_BRANCH_NAME="pr$TRAVIS_PULL_REQUEST"
+
+git -C $LOCAL_PATH fetch origin pull/$TRAVIS_PULL_REQUEST/head:$PULL_REQUEST_BRANCH_NAME
+
+PULL_REQUEST_COMMIT_MESSAGE="Merge \"$TRAVIS_PULL_REQUEST_BRANCH\" -> \"$TRAVIS_BRANCH\" by $USER"
+
+git -C $LOCAL_PATH merge --no-ff -m "$PULL_REQUEST_COMMIT_MESSAGE" $PULL_REQUEST_BRANCH_NAME
+git -C $LOCAL_PATH push -f -q -u "https://$git_hub_personal_access_token@github.com/$TRAVIS_REPO_SLUG" $TRAVIS_BRANCH
--- buildSrc/src/main/resources/travis/bash/gh_pages_report.sh
+++ buildSrc/src/main/resources/travis/bash/gh_pages_report.sh
@@ -7,7 +7,7 @@ git clone --depth=1 --branch=gh-pages $REPO_URL.git $LOCAL_PATH || exit 1
 git -C $LOCAL_PATH config user.name "$USER"
 git -C $LOCAL_PATH config user.email "$USER"
 
-testCoverageSignature=$(<"${TRAVIS_BUILD_DIR}/build/reports/coverage/signature")
+testCoverageSignature=$(<"$TRAVIS_BUILD_DIR/build/reports/coverage/signature")
 testCoveragePath="$LOCAL_PATH/reports/coverage/$testCoverageSignature"
 TEST_COVERAGE_REPORT_URL="$GIT_HUB_PAGES_URL/reports/coverage/$testCoverageSignature"
 
@@ -18,45 +18,45 @@ else
 	echo "make dir $testCoveragePath..."
 	mkdir -p $testCoveragePath
 	echo "move test coverage report"
-	mv ${TRAVIS_BUILD_DIR}/build/reports/coverage/html/* $testCoveragePath
+	mv $TRAVIS_BUILD_DIR/build/reports/coverage/html/* $testCoveragePath
 
 	git -C $LOCAL_PATH add --all .
-	git -C $LOCAL_PATH commit -m "add ${testCoverageSignature} test coverage report"
-	git -C $LOCAL_PATH push -f -q -u "https://${git_hub_personal_access_token}@github.com/${TRAVIS_REPO_SLUG}" gh-pages
+	git -C $LOCAL_PATH commit -m "add $testCoverageSignature test coverage report"
+	git -C $LOCAL_PATH push -f -q -u "https://$git_hub_personal_access_token@github.com/$TRAVIS_REPO_SLUG" gh-pages
 fi
 
-testingSignature=$(<"${TRAVIS_BUILD_DIR}/build/reports/testing/signature")
-testingPath="$LOCAL_PATH/reports/testing/${testingSignature}"
-TESTING_REPORT_URL="$GIT_HUB_PAGES_URL/reports/testing/${testingSignature}"
+testingSignature=$(<"$TRAVIS_BUILD_DIR/build/reports/testing/signature")
+testingPath="$LOCAL_PATH/reports/testing/$testingSignature"
+TESTING_REPORT_URL="$GIT_HUB_PAGES_URL/reports/testing/$testingSignature"
 
 if [ -d $testingPath ]
 then
-    echo "test report by ${testingSignature} already in $LOCAL_PATH"
+    echo "test report by $testingSignature already in $LOCAL_PATH"
 else
 	echo "make dir $testingPath..."
 	mkdir -p $testingPath
 	echo "move test report"
-	mv ${TRAVIS_BUILD_DIR}/build/reports/testing/html/* $testingPath
+	mv $TRAVIS_BUILD_DIR/build/reports/testing/html/* $testingPath
 
 	git -C $LOCAL_PATH add --all .
-	git -C $LOCAL_PATH commit -m "add ${testingSignature} test report"
-	git -C $LOCAL_PATH push -f -q -u "https://${git_hub_personal_access_token}@github.com/${TRAVIS_REPO_SLUG}" gh-pages
+	git -C $LOCAL_PATH commit -m "add $testingSignature test report"
+	git -C $LOCAL_PATH push -f -q -u "https://$git_hub_personal_access_token@github.com/$TRAVIS_REPO_SLUG" gh-pages
 fi
 
-documentationSignature=$(<"${TRAVIS_BUILD_DIR}/build/documentation/signature")
-documentationPath="$LOCAL_PATH/documentation/${documentationSignature}"
-DOCUMENTATION_URL="$GIT_HUB_PAGES_URL/documentation/${documentationSignature}"
+documentationSignature=$(<"$TRAVIS_BUILD_DIR/build/documentation/signature")
+documentationPath="$LOCAL_PATH/documentation/$documentationSignature"
+DOCUMENTATION_URL="$GIT_HUB_PAGES_URL/documentation/$documentationSignature"
 
 if [ -d $documentationPath ]
 then
-    echo "documentation by ${documentationSignature} already in $LOCAL_PATH"
+    echo "documentation by $documentationSignature already in $LOCAL_PATH"
 else
 	echo "make dir $documentationPath..."
 	mkdir -p $documentationPath
 	echo "move documentation"
-	mv ${TRAVIS_BUILD_DIR}/build/documentation/html/* $documentationPath
+	mv $TRAVIS_BUILD_DIR/build/documentation/html/* $documentationPath
 
 	git -C $LOCAL_PATH add --all .
-	git -C $LOCAL_PATH commit -m "add ${testingSignature} documentation"
-	git -C $LOCAL_PATH push -f -q -u "https://${git_hub_personal_access_token}@github.com/${TRAVIS_REPO_SLUG}" gh-pages
+	git -C $LOCAL_PATH commit -m "add $testingSignature documentation"
+	git -C $LOCAL_PATH push -f -q -u "https://$git_hub_personal_access_token@github.com/$TRAVIS_REPO_SLUG" gh-pages
 fi
