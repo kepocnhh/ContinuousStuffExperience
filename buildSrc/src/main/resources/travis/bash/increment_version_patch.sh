5a6f0f1af54787ef3181c9ac81ae6f96fcaae4bc
Stanley Wintergreen <kepocnhh@gmail.com>
2019-10-23 18:54:43 +0300

    fix script 005...

--- buildSrc/src/main/resources/travis/bash/increment_version_patch.sh
+++ buildSrc/src/main/resources/travis/bash/increment_version_patch.sh
@@ -12,8 +12,8 @@ if [ -z "${LOCAL_PATH//$' '/""}" ]; then
   exit 1
 fi
 
-modules=$(gradle -p $LOCAL_PATH -q subprojects)
-lines=$(git -C $LOCAL_PATH diff --name-only origin/$TRAVIS_BRANCH $TRAVIS_BRANCH)
+modules=($(gradle -p $LOCAL_PATH -q subprojects))
+lines=($(git -C $LOCAL_PATH diff --name-only origin/$TRAVIS_BRANCH $TRAVIS_BRANCH))
 
 if [ -z "${modules//$' '/""}" ] || [ ${#modules[@]} -eq 0 ]; then
   echo "modules must be not empty"
@@ -31,7 +31,7 @@ for line in ${lines[@]}; do
   isDiffInModule=0
   for module in ${modules[@]}; do
     if [[ $line == "$module/"* ]]; then
-      gradle -q :$module:incrementVersionPatch
+      gradle -p $LOCAL_PATH -q :$module:incrementVersionPatch
       echo "version of \"$module\" module incremented"
       for i in "${!modules[@]}"; do
         if [[ ${modules[i]} == $module ]]; then
@@ -43,7 +43,7 @@ for line in ${lines[@]}; do
     fi
   done
   if [ $rootVersionIncremented -eq 0 ] && [ $isDiffInModule -eq 0 ]; then
-    gradle -q :incrementVersionPatch
+    gradle -p $LOCAL_PATH -q :incrementVersionPatch
     echo "version of root module incremented"
     rootVersionIncremented=1
   fi
@@ -53,14 +53,14 @@ for line in ${lines[@]}; do
   fi
 done
 
-git -C $LOCAL_PATH add --all . || ILLEGAL_STATE=$?
+git -C $LOCAL_PATH -q add --all . || ILLEGAL_STATE=$?
 if [ $ILLEGAL_STATE -ne 0 ]
 then
   echo "adding failed!"
   exit $ILLEGAL_STATE
 fi
 
-git -C $LOCAL_PATH commit -m "Increment version patch by $USER" || ILLEGAL_STATE=$?
+git -C $LOCAL_PATH -q commit -m "Increment version patch by $USER" || ILLEGAL_STATE=$?
 if [ $ILLEGAL_STATE -ne 0 ]
 then
   echo "commiting failed!"
